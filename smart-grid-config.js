8ea711e456753bb0be1657c54309c0bfb297795d
nurs-dev <nursultan_nurgaliyev@protonmail.com>
2020-11-11 12:54:31 +0600

    smart-grid: configured

--- smart-grid-config.js
+++ smart-grid-config.js
@@ -2,7 +2,7 @@ const smartgrid = require('smart-grid');
 
 /* It's principal settings in smart grid project */
 const settings = {
-	outputStyle: 'sass',
+	outputStyle: 'scss',
 	/* less || scss || sass || styl */
 	columns: 12,
 	/* number of grid columns */
@@ -13,22 +13,24 @@ const settings = {
 	container: {
 		maxWidth: '1200px',
 		/* max-width оn very large screen */
-		fields: '30px' /* side fields */
+		fields: '15px' /* side fields */
 	},
 	breakPoints: {
+		xl: {
+			width: "1200px"
+		/* -> @media (max-width: 1200px) */
+		},
 		lg: {
-			width: '1100px',
-			/* -> @media (max-width: 1100px) */
+			width: "992px"
 		},
 		md: {
-			width: '960px'
+			width: "768px"
 		},
 		sm: {
-			width: '780px',
-			fields: '15px' /* set fields only if you want to change container.fields */
+			width: "576px"
 		},
 		xs: {
-			width: '560px'
+			width: "320px"
 		}
 		/* 
 		We can create any quantity of break points.
