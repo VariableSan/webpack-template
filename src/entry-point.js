41a7eccd78c7d71ec39746fe7d1af1cda5f7fbc2
nurs-dev <nursultan_nurgaliyev@protonmail.com>
2021-03-17 22:29:34 +0600

    Package: added vue-axios

--- src/entry-point.js
+++ src/entry-point.js
@@ -11,6 +11,8 @@ import Vue from 'vue'
 import router from './plugins/router/router'
 import vuetify from './plugins/vuetify'
 import store from './plugins/store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 /*==================== VUE DEPENDENCY END====================*/
 
 /*==================== DEPENDENCY LIBRARIES START====================*/
@@ -22,6 +24,8 @@ import App from './layouts/App.vue'
 
 // Vue components (for use in html)
 // Vue.component('example-component', require('./components/Example.vue').default)
+
+Vue.use(VueAxios, axios)
 
 // Vue init
 const app = new Vue({
